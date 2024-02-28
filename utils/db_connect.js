5b99b9e11a2061c2eb13669a659533ea64e021de
Evan Kasky <ekasky25@gmail.com>
2024-02-27 22:42:26 -0500

    Moves Sequelize instance from middleware to util

--- utils/db_connect.js
+++ utils/db_connect.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-const { db_name, db_user, db_pass, db_host, db_dialect } = require("../utils/config");
+const { db_name, db_user, db_pass, db_host, db_dialect } = require("./config");
 
 const sequelize = new Sequelize(db_name, db_user, db_pass, {
     host: db_host,
