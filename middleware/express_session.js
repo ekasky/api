4de39673cc4eb03eb2fc06546d6a6057c1a7cd3a
Evan Kasky <ekasky25@gmail.com>
2024-02-28 14:33:09 -0500

    Fixes cookie time in sessions table

--- middleware/express_session.js
+++ middleware/express_session.js
@@ -1,12 +1,15 @@
 const session = require("express-session");
 const MySQLStore = require("express-mysql-session")(session);
-const { db_host, db_user, db_pass, db_name, db_session_secret } = require("../utils/config");
+const { db_host, db_user, db_pass, db_name, db_session_secret,  db_session_expire_time } = require("../utils/config");
 
 const sessionStore = new MySQLStore({
     host: db_host,
     user: db_user,
     password: db_pass,
-    database: db_name
+    database: db_name,
+    clearExpired: true,
+    checkExpirationInterval:  db_session_expire_time,
+    expiration: db_session_expire_time
 });
 
 const express_session = session({
@@ -17,8 +20,9 @@ const express_session = session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 1000,
+        maxAge:  db_session_expire_time,
         httpOnly: true,
+        sameSite: true
     }
 
 });
--- utils/config.js
+++ utils/config.js
@@ -9,6 +9,7 @@ module.exports = {
     db_pass: process.env.DB_PASS,
     db_host: process.env.DB_HOST,
     db_dialect: "mysql",
-    db_session_secret: process.env.DB_SESSION_SECRET
+    db_session_secret: process.env.DB_SESSION_SECRET,
+    db_session_expire_time: 60000                                                               // One minute
 
 };
