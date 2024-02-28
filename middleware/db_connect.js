838426a10aece24654cc5edcb35068a7e8aea375
Evan Kasky <ekasky25@gmail.com>
2024-02-27 22:40:18 -0500

    Creates a Sequelize Instance

---
+++ middleware/db_connect.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require("sequelize");
+const { db_name, db_user, db_pass, db_host, db_dialect } = require("../utils/config");
+
+const sequelize = new Sequelize(db_name, db_user, db_pass, {
+    host: db_host,
+    dialect: db_dialect
+});
+
+module.exports = sequelize;
