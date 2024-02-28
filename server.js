15bdb62cf30d10454e39bc0141addc7bda28d5c6
Evan Kasky <ekasky25@gmail.com>
2024-02-28 00:06:43 -0500

    Uses cookie parser middleware as well as the session store

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const { server_port } = require("./utils/config");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const express_session = require("./middleware/express_session");
 const app = express();
 
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(express_session);
 
 app.use("/api/auth", authRoutes);
 
