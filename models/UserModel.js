f0becd951a2379232f4043d6c29a39f1aa31e33e
Evan Kasky <ekasky25@gmail.com>
2024-02-27 23:17:49 -0500

    Adds sign up method to the User Model

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,5 +1,7 @@
 const sequelize = require("../utils/sequelize");
 const { Sequelize, DataTypes } = require("sequelize");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 
 const User = sequelize.define("User", {
@@ -36,5 +38,47 @@ const User = sequelize.define("User", {
     timestamps: true,
     underscored: true,
 });
+
+// Sign Up User Method
+User.signup = async (first_name, last_name, username, email, password) => {
+
+  // Ensure all the required fields are present
+  if( !first_name || !last_name || !username || !email || !password ) {
+    throw new Error("All feilds required");
+  }
+
+  // Ensure email is a valid email
+  if(!validator.isEmail(email)) {
+    throw new Error("Invalid Email");
+  }
+
+  // Ensure password is strong
+  if(!validator.isStrongPassword(password)) {
+    throw new Error("Password not strong enough");
+  }
+
+  // Ensure email and/or username is not already in use
+  const existingUser = await User.findOne({ where: { [Sequelize.Op.or]: [{username}, {email}] } });
+
+  if(existingUser) {
+    throw new Error("Username or email already Exists");
+  }
+
+  // Hash the users password
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  // Create new user
+  const user = await User.create({
+    first_name,
+    last_name,
+    username,
+    email,
+    password: hash
+  });
+
+  return user;
+
+}
 
 module.exports = User;
