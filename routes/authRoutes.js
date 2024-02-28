420651211ca2d59cc8bdea5521efdc58813caf79
Evan Kasky <ekasky25@gmail.com>
2024-02-28 15:05:19 -0500

    Adds route to change role

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const signupController = require("../controllers/signupController");
 const loginController = require("../controllers/loginController");
+const changeRoleController = require("../controllers/changeRoleController");
+
+const isAdmin = require("../middleware/is_admin");
 
 router.post("/signup", signupController);
 router.post("/login", loginController);
+router.post("/change-role", isAdmin, changeRoleController);
 
 module.exports = router;
