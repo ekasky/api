309dbf6899d5630c82e76647cccde24571876bde
Evan Kasky <ekasky25@gmail.com>
2024-02-28 20:12:16 -0500

    Adds route for logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const signupController = require("../controllers/signupController");
 const loginController = require("../controllers/loginController");
+const logoutController = require("../controllers/logoutController");
 const changeRoleController = require("../controllers/changeRoleController");
 
 const isAdmin = require("../middleware/is_admin");
 
 router.post("/signup", signupController);
 router.post("/login", loginController);
+router.post("/logout", logoutController);
 router.post("/change-role", isAdmin, changeRoleController);
 
 module.exports = router;
